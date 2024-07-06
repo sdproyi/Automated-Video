76373d94e19175a4d1798facffce222ce68d37da
sdproyi <sdprohaxer@gmail.com>
2024-07-06 17:28:44 +0200

    fix for git to not upload heavy files from public/

--- Video-generator/src/features/Avatar.tsx
+++
@@ -1,9 +0,0 @@
-import { Img, staticFile } from "remotion";
-
-export const Avatar = () => {
-  return (
-    <div className="w-1/5 left-14 bottom-0 absolute">
-      <Img src={staticFile("avatar.png")} />
-    </div>
-  );
-}
--- Video-generator/src/webpack-override.ts
+++
@@ -1,6 +0,0 @@
-import {enableTailwind} from '@remotion/tailwind';
-import type {WebpackOverrideFn} from '@remotion/bundler';
-
-export const webpackOverride: WebpackOverrideFn = (currentConfiguration) => {
-	return enableTailwind(currentConfiguration);
-};
